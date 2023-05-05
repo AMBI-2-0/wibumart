4a263da691b45baddb9544a01cd74c3532a13992
Mabouya04 <iilbarcelona9@gmail.com>
2023-05-05 21:26:32 +0800

    Init Login & Registration Form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,55 @@
 @extends('layouts.master')
 
 @section('content')
-        <div class="carousel slide" data-ride="carousel">
-                
+
+
+    <div class="carousel slide" data-ride="carousel">
+
         <div class="carousel-inner">
-                <div class="carousel-item active">
-                        <img class="d-block w-100" src="https://disk.mediaindonesia.com/files/news/2022/12/30/WhatsApp%20Image%202022-12-22%20at%2017.07.10%20(1).jpg" alt="First slide">
-                </div>
-                <div class="carousel-item">
-                        <img class="d-block w-100" src="https://disk.mediaindonesia.com/files/news/2022/12/30/WhatsApp%20Image%202022-12-22%20at%2017.06.59.jpg" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                        <img class="d-block w-100" src="https://disk.mediaindonesia.com/files/news/2022/12/30/WhatsApp%20Image%202022-12-22%20at%2017.06.50.jpg" alt="Third slide">
-                </div>
+            <div class="carousel-item active">
+                <img class="d-block w-100 mx-auto"
+                    src="https://disk.mediaindonesia.com/files/news/2022/12/30/WhatsApp%20Image%202022-12-22%20at%2017.07.10%20(1).jpg"
+                    alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100 mx-auto"
+                    src="https://disk.mediaindonesia.com/files/news/2022/12/30/WhatsApp%20Image%202022-12-22%20at%2017.06.59.jpg"
+                    alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100 mx-auto"
+                    src="https://disk.mediaindonesia.com/files/news/2022/12/30/WhatsApp%20Image%202022-12-22%20at%2017.06.50.jpg"
+                    alt="Third slide">
+            </div>
         </div>
-                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         </a>
-      </div>
-<div class="container ">
-        
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </a>
+    </div>
+    <div class="container">
+
         @for ($i = 0; $i < 3; $i++)
-                
-                   <div class="row">
-                                @for ($a = 0; $a < 4; $a++)
-                                        <div class="col-sm">
-                                                <div class="card mt-5 border border-tertiary" style="width: 10rem;">
-                                                        <img class="card-img-top" src="https://foto.kontan.co.id/U9uyqRKY07FwuFwPeYscphOpZbA=/smart/2022/11/04/1057840375p.jpg" alt="Card image cap">
-                                                        <div class="card-body bg-dark">
-                                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                        </div>
-                                                </div>
-                                        </div>
-                
-                        @endfor 
-                   </div> 
+            <div class="row">
+                @for ($a = 0; $a < 4; $a++)
+                    <div class="col-sm">
+                        <div class="card mt-5 border border-tertiary" style="width: 10rem;">
+                            <img class="card-img-top"
+                                src="https://foto.kontan.co.id/U9uyqRKY07FwuFwPeYscphOpZbA=/smart/2022/11/04/1057840375p.jpg"
+                                alt="Card image cap">
+                            <div class="card-body bg-dark " style="color:azure">
+                                <h5 class="card-title">
+                                        Figur Anya
+                                </h5>
+                                <h6 class="card-text-subtitle">Sebuah figur anya</h6>
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+            </div>
         @endfor
-                </div>            
-                
+    </div>
+
 @endsection
-    
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,116 +1,116 @@
 <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Wibumart</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            @if ($title == "Figurine Page")
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#"> Figurine</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Clothing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Props</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Accessories</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Books</a>
-              </li>
-              @endif
-            @if ($title == "Clothing Page")
-              <li class="nav-item">
-                <a class="nav-link" href="#"> Figurine</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Clothing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Props</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Accessories</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Books</a>
-              </li>
-              @endif
-            @if ($title == "Props Page")
-              <li class="nav-item">
-                <a class="nav-link" href="#"> Figurine</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Clothing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Props</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Accessories</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Books</a>
-              </li>
-              @endif
-            @if ($title == "Accessories Page")
-              <li class="nav-item">
-                <a class="nav-link" href="#"> Figurine</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Clothing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Props</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Accessories</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Books</a>
-              </li>
-              @endif
-            @if ($title == "Books Page")
-              <li class="nav-item">
-                <a class="nav-link" href="#"> Figurine</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Clothing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Props</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Accessories</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Books</a>
-              </li> 
+        <a class="navbar-brand" href="#">Wibumart</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                @if ($title == 'Figurine Page')
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"> Figurine</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Clothing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Props</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Accessories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Books</a>
+                    </li>
+                @endif
+                @if ($title == 'Clothing Page')
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Figurine</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Clothing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Props</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Accessories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Books</a>
+                    </li>
+                @endif
+                @if ($title == 'Props Page')
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Figurine</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Clothing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Props</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Accessories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Books</a>
+                    </li>
+                @endif
+                @if ($title == 'Accessories Page')
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Figurine</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Clothing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Props</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Accessories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Books</a>
+                    </li>
+                @endif
+                @if ($title == 'Books Page')
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Figurine</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Clothing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Props</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Accessories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Books</a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Figurine</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Clothing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Props</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Accessories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Books</a>
+                    </li>
+                @endif
 
-              @else
-               <li class="nav-item">
-                <a class="nav-link" href="#"> Figurine</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Clothing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Props</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Accessories</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Books</a>
-              </li> 
-              @endif
-          
-            <a href="login" class="btn btn-outline-light d-flex">Login</a>
-        </ul>
-      </div>
+                <a href="login" class="btn btn-outline-light d-flex">Login</a>
+            </ul>
+        </div>
     </div>
-  </nav>
+</nav>

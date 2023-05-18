a61bdc063b6d16ca71b7f08fafea24e4e91f677a
Yun He <2010817110012@mhs.ulm.ac.id>
2023-05-18 15:22:58 +0800

    ADD: Database Relation and Auth for Cart Page

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function keranjang_detail()
+    {
+        return $this->hasMany('App\KeranjangDetail','product_id', 'id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -14,4 +14,8 @@ class User extends Authenticatable
 
     protected $guarded = ['id'];
 
+    public function keranjang()
+    {
+        return $this->hasMany('App\Keranjang','users_id', 'id');
+    }
 }

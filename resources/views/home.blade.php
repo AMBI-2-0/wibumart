d3f7652e14e2da10b465c4595cef7a1697b86d90
Yun He <2010817110012@mhs.ulm.ac.id>
2023-05-20 22:42:51 +0800

    Change: View Home From Database Product

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,36 +96,26 @@
 
     <div class="container-fluid">
         <div class="row">
-            <?php
-            for ($i = 1; $i <= 8; $i++) {
-                echo '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 mt-4">';
-                echo '<div class="card bg-dark text-white border-0">';
-                echo '<img src="/images/caro-item-1.png" class="card-img-top" alt="...">';
-                echo '<div class="card-body">';
-                echo "<h5 class='card-title'>Card $i</h5>";
-                echo "<p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content. $i</p>";
-                echo '</div>';
-                echo '</div>';
-                echo '</div>';
-            }
-            ?>
-        </div>
-    </div>
-    <div class="container-fluid">
-        <div class="row">
-            <?php
-            for ($i = 1; $i <= 24; $i++) {
-                echo '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 mt-4">';
-                echo '<div class="card bg-dark text-white border-0">';
-                echo '<img src="/images/caro-item-1.png" class="card-img-top" alt="...">';
-                echo '<div class="card-body">';
-                echo "<h5 class='card-title'>Card $i</h5>";
-                echo "<p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content. $i</p>";
-                echo '</div>';
-                echo '</div>';
-                echo '</div>';
-            }
-            ?>
+
+            @foreach ($products as $product)
+                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 mt-4">
+                    <div class="card bg-dark text-white border-0">
+                        <img src="/images/caro-item-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class='card-title'>{{ $product->nama_product }}</h5>
+                            <p class='card-text'>
+                                <strong>Harga : </strong> Rp. {{ number_format($product->price) }} <br>
+                                <strong>Stok :</strong> {{ $product->jumlah_product }} <br>
+                                <strong>Kategori : </strong> {{ $product->kategori_product }} <br>
+                                <hr>
+                                <strong>Keterangan : </strong> <br>
+                                {{ $product->description }}
+                            </p>
+                            <a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Pesan</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
